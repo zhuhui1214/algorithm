567991a7bffac2afd5a8669484349927f0c30d19
zhuhui1214 <32250937+zhuhui1214@users.noreply.github.com>
2018-09-25 14:48:33 +0800

    Update main.cpp

--- subString/main.cpp
+++ subString/main.cpp
@@ -1,4 +1,4 @@
-class Solution {
+
 public:
     int lengthOfLongestSubstring(string s) {
         if(s==""){return 0;}
@@ -8,12 +8,13 @@ public:
                 string tmp1 = s.substr(i,j-i);//从第一个循环的第一个点开始，找到j-i个
                 string tmp2 = s.substr(j,1);//从第二个循环开始只要找1个
                 std::size_t found = tmp1.find(tmp2);
-                if (found!=std::string::npos){//找到了,马上要执行一个break
-                    if(j-i>bigresult)
-                    {bigresult =  j-i;}
-                    break;//break 跳出第2个循环，然后继续第一个循环
+                if (found==std::string::npos){//没找到了,一直循环下去，在循环前，需要更新bigresult的值
+                    if(j-i+1>bigresult)
+                    {bigresult =  j-i+1;}
                 }//if
-                else
+                else{
+                    break;//break 跳出第2个循环，然后继续第一个循环
+                }
             }//for second
         }//for first
         return bigresult;
